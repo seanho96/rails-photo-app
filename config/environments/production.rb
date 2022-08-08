9c38c391462188fc4db4d5bba556322a975cc960
Sean Ho <seanho96@yahoo.com>
2022-08-08 09:09:51 +0800

    chore: update host and port to heroku env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { host: 'rails-photoapp.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['HOST'], port: ENV['PORT'] }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
