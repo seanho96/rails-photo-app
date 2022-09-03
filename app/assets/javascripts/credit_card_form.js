7194791d474e4ef26268e589c6660c262df6ddb9
Sean Ho <seanho96@yahoo.com>
2022-09-03 10:03:03 +0800

    refactor: exit early to prevent calling the DOM

--- app/assets/javascripts/credit_card_form.js
+++ app/assets/javascripts/credit_card_form.js
@@ -2,17 +2,17 @@ $(document).on("ready turbolinks:load", function () {
   let show_error, stripeResponseHandler, submitHandler;
 
   submitHandler = function (event) {
+    if (!Stripe) {
+      show_error(
+        "Failed to load credit card processing functionality. Please reload this page in your browser."
+      );
+      return;
+    }
     const $form = $(event.target);
     $form.find("input[type=submit]").prop("disabled", true);
 
     //If Stripe was initialized correctly this will create a token using the credit card info
-    if (Stripe) {
-      Stripe.card.createToken($form, stripeResponseHandler);
-    } else {
-      show_error(
-        "Failed to load credit card processing functionality. Please reload this page in your browser."
-      );
-    }
+    Stripe.card.createToken($form, stripeResponseHandler);
 
     return false;
   };
